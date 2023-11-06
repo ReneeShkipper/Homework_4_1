99450e81bbe393d5a8aa7d95c595dc812cfeb9b3
Renee <148131324+ReneeShkipper@users.noreply.github.com>
2023-11-06 14:53:47 +0300

    Update Program.cs

--- Homework_4_1/Program.cs
+++ Homework_4_1/Program.cs
@@ -4,11 +4,7 @@ Console.WriteLine("Fill the array");
 int[] array = new int[N];
 int sum = 0;
 int num = 0;
-for (int i = 0; i < N; i++) 
-{
-    array[i] = int.Parse(Console.ReadLine());
-}
-
+for (int i = 0; i < N; i++) array[i] = int.Parse(Console.ReadLine());
 foreach (int j in array)
 {
     sum = j > 0 ? sum + j : sum;
